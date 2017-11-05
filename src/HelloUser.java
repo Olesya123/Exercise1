6a06076c9e4765bfc45b4c2472596df94201d392
Olesya123 <eidam_olesya@yahoo.de>
2017-11-05 20:43:02 +0100

    Hello User is added

---
+++ src/HelloUser.java
@@ -0,0 +1,12 @@
+
+public class HelloUser {
+	public static String name;
+
+	public HelloUser(String newUser) {
+		name = newUser;
+	}
+	public static void greetUser(String name){
+		System.out.println("hello "+name);
+		
+	}
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.println("Whats your name?");
+		Scanner in = new Scanner(System.in);
+		String userName = in.nextLine();
+		HelloUser neu = new HelloUser(userName);
+		neu.greetUser(neu.name);
 	}
 
 }
